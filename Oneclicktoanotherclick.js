440ebed03f683e7c496f32b2efc4551b6a3a9781
Syed Hussain Abbas Zaidi <60025851+syedhussainabbaszaidi@users.noreply.github.com>
2023-10-25 16:02:29 +0500

    Oneclicktoanotherclick

---
+++ Oneclicktoanotherclick.js
@@ -0,0 +1,14 @@
+// http://fineminent.com/
+
+jQuery('ul#ast-hf-menu-1>li:nth-child(1)>a').on('click', function () {
+    jQuery('nav.docSlider-pager>button:nth-child(1)').click();
+});
+jQuery('ul#ast-hf-menu-1>li:nth-child(2)>a').on('click', function () {
+    jQuery('nav.docSlider-pager>button:nth-child(2)').click();
+});
+jQuery('ul#ast-hf-menu-1>li:nth-child(3)>a').on('click', function () {
+    jQuery('nav.docSlider-pager>button:nth-child(3)').click();
+});
+jQuery('ul#ast-hf-menu-1>li:nth-child(4)>a').on('click', function () {
+    jQuery('nav.docSlider-pager>button:nth-child(4)').click();
+});
